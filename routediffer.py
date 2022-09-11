1790d425f66505a8436fdaf39ac67031b7a464cd
Scotty <mykrazyemail@protonmail.com>
2022-09-11 14:22:40 -0500

    Building out more logic

--- routediffer.py
+++ routediffer.py
@@ -16,22 +16,34 @@ nxos_headers: list = ["vrf", "protocol", "type", "network", "mask", "distance", 
 ios_headers: list = ["protocol", "type", "network", "mask", "distance", "metric", "nexthop_ip", "nexthop_if", "uptime"]
 
 
-nexus = {
-    "device_type": "cisco_nxos",
-    "host": "192.168.217.2",
-    "username": "admin",
-    "password": "admin"
-}
+def get_routing_table(ipaddr: str = None, credential_id: str = None, platform: str = None):
 
-def get_routing_table(profile):
+    # run the import statemements from within the function as it makes it easier to 
+    # work with the loggging in
+    from client import SSHClient
+    from ciscocmds import CiscoCommands
 
-    with ConnLogOnly(**profile) as connection:
+    # lets make sure the required information has been provide and if not
+    # try and gracefully handle that
+    if ipaddr == None or credential_id == None or platform == None:
 
-        routing_table = connection.send_command("show ip route", use_textfsm=True, strip_prompt=True, strip_command=True)
-        connection.disconnect()
+        raise ValueError("IP Address, Credential ID and Platorm Type are required")
+    
+    else:
+
+        # if there were no exceptions, everything needed should be preset therefore proceed with
+        # the log in to get the routing table
+        ssh_client = SSHClient(hostname=ipaddr, profile=credential_id, platform=platform)
+        ssh_client.ssh_host_login()
+
+        routing_table = ssh_client.session.send_command(CiscoCommands.show_routes(), use_textfsm=True, strip_prompt=True, strip_command=True)
+
+        # disconnect from the client
+        ssh_client.session.disconnect()
+
         return routing_table
 
-def create_master_route_table(routes, headers):
+def create_master_route_table(routes: json, headers: list = []):
 
     with open("master_route_table.csv", "w") as master_copy:
 
@@ -52,11 +64,19 @@ def create_master_route_table(routes, headers):
                 "nexthop_vrf": route["nexthop_vrf"],
             })
 
-
 # if the master routing table does not already exist, it needs to be created
 if not Paths.file_exists("master_route_table.csv"):
-    routing_table = get_routing_table(profile=nexus)
-    create_master_route_table(routes=routing_table, headers=nxos_headers)
+
+    try:
+        routing_table = get_routing_table(ipaddr="192.168.217.2", credential_id="nxos", platform="cisco_nxos")
+    except ValueError as e:
+        print(e)
+    except ImportError as e:
+        print(e)
+    else:
+        # if there are no errors from getting the routing table, then parse it out into the CSV file
+        create_master_route_table(routes=routing_table, headers=nxos_headers)
+
 else:
     pass
     
