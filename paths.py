b187f27478539451e11e83dfdeeaf526b60d1c66
Scotty <mykrazyemail@protonmail.com>
2022-09-11 13:18:34 -0500

    Updated docstrings

--- paths.py
+++ paths.py
@@ -3,10 +3,12 @@ import pathlib
 
 class Paths:
     """
-     Static methods that point to various resources within the project folder
+     Various methods for working with paths within the project folder
 
-    Returns:
-        path: root path and paths to various project resources
+        root_path: returns the root path to the working folder
+
+        file_exists: returns True or False depeding on the existence of the
+        filename provided
     """
 
     @staticmethod
@@ -18,6 +20,13 @@ class Paths:
         return pathlib.Path(filename).exists()
 
     def create_file(filename):
+        """
+        create_file summary:
+            Creates a file of the filename provided
+
+        Args:
+            filename (str): The file name to be created
+        """
 
         try:
             if not pathlib.Path(filename).is_file():
@@ -27,6 +36,13 @@ class Paths:
             return f"Error: {e.strerror}"
 
     def delete_file(filename):
+        """
+        delete_file summary:
+            Deletes a file of the filename provided if it exists
+
+        Args:
+            filename (str): The file name to be deleted
+        """
         
         try:
             if pathlib.Path(filename).exists():
@@ -35,6 +51,13 @@ class Paths:
             return f"Error: {e.strerror}"
 
     def create_folder(folder):
+        """
+        create_folder summary:
+            Creates a folder of the name provided
+
+        Args:
+            folder (str): The folder name to be created
+        """
 
         try:
             if not pathlib.Path(folder).is_dir():
@@ -44,6 +67,13 @@ class Paths:
             return f"Error: {e.strerror}"
 
     def delete_folder(folder):
+        """
+        delete_folder summary:
+            Deletes/Removes a folder of the name provided if it exists
+
+        Args:
+            folder (str): The folder name to be created
+        """
 
         try:
             if pathlib.Path(folder):
