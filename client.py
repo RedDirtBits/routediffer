862a40fd16578c699d32cc5cb90e563623b484ec
Scotty <mykrazyemail@protonmail.com>
2022-09-17 20:24:41 -0500

    Updated to work with credentials dataclass

--- client.py
+++ client.py
@@ -1,8 +1,31 @@
 import logging
 import sys
 import os
-from dotenv import load_dotenv
+from dataclasses import dataclass, field
 from netmiko import ConnLogOnly
+from dotenv import load_dotenv
+
+
+@dataclass
+class LoginCredentials:
+    """
+     A dataclass that pulls a set of credentials from a user provided .env
+     file containing a prefix that uniquely identifies a particular set of
+     credentials over another
+    """
+
+    load_dotenv()
+
+    credential_id: str
+    username: str = field(default="")
+    password: str = field(default="")
+    enable: str = field(default="")
+
+    def __post_init__(self):
+
+        self.username = os.environ.get(f"{self.credential_id}_username")
+        self.password = os.environ.get(f"{self.credential_id}_password")
+        self.enable = os.environ.get(f"{self.credential_id}_enable")
 
 
 class SSHClient:
@@ -13,43 +36,44 @@ class SSHClient:
         hostname (str):
             The hostname or IP address of the network device being logged into
 
-        profile (str):
-            The profile is a prefix for the user provided .env file which is loaded
-            and read by the third-party library python-dotenv.  The load_dotenv()
-            method is initialized as part of the class init.
+        credential_id (str):
+            The credential_id is a prefix for the user provided .env file that
+            uniquely identifies a set of credentials out of all that may exist
+            within the .env file
 
             This allows for flexibility for when one may work in different environments
             where SSH login credentials may be different.  For example, testing,
             production, etc.
 
             This assumes the .env file has been written so that all credentials are written
-            in a uniform manner using a uniform prefix to uniquely identify the environment
+            in a uniform manner using a prefix to identify the particular credentials
+            needed.
 
-            Example: testing_username, testing_password, testing_secret
+            For example: testing_username, testing_password, testing_secret
 
-            In this case, the user would set the profile argument to "testing"
+            In this case, the user would set the credential_id argument to "testing"
 
-        platform (str):
+        platform_id (str):
             The Netmiko platform being connected to.  Can be cisco_ios for IOS
             devices, cisco_nxos for Nexus devices, cisco_telnet for when using
             Telnet, etc.
     """
 
-    def __init__(self, hostname: str, profile: str, platform: str) -> None:
+    def __init__(self, hostname: str, credential_id: str, platform_id: str) -> None:
 
         self.hostname = hostname
-        self.profile = profile
-        self.platform = platform
+        self.credential_id = credential_id
+        self.platform_id = platform_id
         self.session = None
 
-        load_dotenv()
+        self.credentials = LoginCredentials(self.credential_id)
 
         self.ssh_profile = {
-            "device_type": self.platform,
+            "device_type": self.platform_id,
             "host": self.hostname,
-            "username": os.environ.get(f"{self.profile}_username"),
-            "password": os.environ.get(f"{self.profile}_password"),
-            "secret": os.environ.get(f"{self.profile}_enable"),
+            "username": self.credentials.username,
+            "password": self.credentials.password,
+            "secret": self.credentials.enable
         }
 
     def ssh_host_login(self):
@@ -73,14 +97,14 @@ class SSHClient:
         # handler if this is encountered
         if self.session is None:
             return sys.exit(f"Unable To Log in to: {self.hostname}")
+        else:
+            self.device_hostname = self.session.find_prompt()[:-1]
 
         # in most cases, particularly in cases using TACACS, we will be dropped into
         # privileged EXEC mode on the device.  However, as a sanity check, let's make
         # sure and if not run enable to get there
         if not self.session.check_enable_mode():
             self.session.enable()
-        else:
-            self.device_hostname = self.session.find_prompt()[:-1]
 
         return self.session
 
